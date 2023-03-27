4d9bb14ed423f85e66b1059454d9f1b4e9beed6e
Ayrton Gonçalves Guerra <ayrtonguerra@pop-os.localdomain>
2023-03-27 02:50:22 -0300

    initial commit of my project vue-router

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheNavigation from "./views/TheNavigation.vue";
+</script>
+
+<template>
+  <div class="container">
+    <TheNavigation></TheNavigation>
+    <RouterView></RouterView>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>All Destinations</h1>
+
+    <div class="destinations">
+      <RouterLink
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="{ name: 'destination.show', params: { id: destination.id } }"
+      >
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import sourceData from "../data.json";
+
+const destinations = sourceData.destinations;
+</script>
