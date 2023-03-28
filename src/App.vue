afa6c20b3cfbd64ba8f620338306a8cfd6110b13
Ayrton Gonçalves Guerra <ayrtonguerra@pop-os.localdomain>
2023-03-28 01:15:45 -0300

    add transitions pages

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,25 @@ import TheNavigation from "./components/TheNavigation.vue";
 </script>
 
 <template>
+  <TheNavigation></TheNavigation>
   <div class="container">
-    <TheNavigation></TheNavigation>
-    <RouterView :key="$route.path"></RouterView>
+    <RouterView v-slot="{ Component }">
+      <Transition name="slide" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </Transition>
+    </RouterView>
   </div>
 </template>
+
+<style>
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 1s, transform 1s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+</style>
