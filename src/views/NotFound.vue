10cc35bf5752908a91478f814db4094249720bea
Ayrton <ayrton.guerra@cliqx.com.br>
2023-08-03 02:25:08 -0300

    feat: notFound page created

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Not Found</h1>
+
+        <p>
+            Oops, we couldn't find that page. Try going
+            <RouterLink to="/">home</RouterLink>
+        </p>
+    </div>
+</template>
