94f2f1ba46ad308596febaae8c919671d4d6f586
Ayrton <ayrton.guerra@cliqx.com.br>
2023-08-03 02:24:28 -0300

    chore: ajusting notfound page and animations

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,36 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import souceData from '../data.json'
 
 const routes = [
   { path: "/", name: "Home", component: Home },
+  {
+    path: '/protected',
+    name: 'Protected',
+    component: () => import('../views/Protected.vue'),
+    meta: {
+      requiresAuth: true
+    
+    }
+  },
   {
     path: "/destionation/:id/:slug",
     name: "destination.show",
     component: () => import("../views/DestinationShow.vue"),
     props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to, from) {
+      const exists = souceData.destinations.find(
+        destination => destination.id === parseInt(to.params.id)
+      )
+      if (!exists) {
+        return { 
+          name: 'NotFound',
+          params: { pathMatch: to.path.split('/').slice(1) },
+          query: to.query,
+          hash: to.hash,
+        }
+      }
+    },
     children: [
       {
         path: ":experienceSlug",
@@ -17,11 +40,27 @@ const routes = [
       },
     ],
   },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import("../views/NotFound.vue")
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior (to, from, savedPosition) {
+    return savedPosition || new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ top: 0, behavior: 'smooth' })
+      }, 300)
+    })
+  },
 });
+
+router.beforeEach((to, from, next) => {
+
+})
 
 export default router;
